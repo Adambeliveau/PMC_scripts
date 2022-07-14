2c1066b56fb780778c37fbc93c36fefa98d2d83f
Adam B <bela1003@usherbrooke.ca>
2022-07-13 22:08:37 -0400

    latest change

--- timelogs/main.py
+++ timelogs/main.py
@@ -1,11 +1,9 @@
 import datetime
-import math
 from datetime import timedelta
 import re
 from os.path import dirname, join
 
 import pandas as pd
-import matplotlib.pyplot as plt
 
 UNITS = {'s':'seconds', 'm':'minutes', 'h':'hours', 'd':'days', 'w':'weeks'}
 splited_worklogs = pd.DataFrame()
@@ -24,22 +22,31 @@ def split_worklogs(row):
 
 worklogs = pd.read_csv(join(dirname(__file__), 'worklogs.csv'), dtype=object)[['Author', 'Issue', 'Time spent seconds']].apply(lambda x: split_worklogs(x), axis=1)
 timesheet = pd.read_csv(join(dirname(__file__), 'timesheet-per-issue.csv'), dtype=object).rename({'Key': 'Issue'}, axis=1).set_index('Issue')
+issues = pd.read_csv(join(dirname(__file__), 'issues.csv'), dtype=object).rename({'Issue key': 'Issue'}, axis=1)[['Summary', 'Issue']].set_index('Issue')
 
 
 def make_timelogs():
     timelogs = pd.concat([timesheet, splited_worklogs], axis=1, join="inner").iloc[:-1, :-1]
+    timelogs.drop(columns=['Summary'], inplace=True)
+    timelogs = issues.join(timelogs, on='Issue', how='left')
+    timelogs.fillna('', inplace=True)
     timelogs.drop(['Total time spent'], axis=1, inplace=True)
     timelogs.rename(columns=lambda x: re.sub(r'Time spent(.*)', 'Total time spent', x), inplace=True)
     timelogs['Estimated time'] = timelogs.apply(lambda x: calculate_estimated_time(x), axis=1)
     timelogs['Progress'] = timelogs['Progress'].apply(lambda x: x + '%')
     timelogs = timelogs.apply(lambda x: add_id_to_summary(x), axis=1)
     timelogs = timelogs[['Summary', 'Total time spent', 'Estimated time', 'Progress', 'Marie-Eve Castonguay', 'Antoine Laberge', 'Simon Pelletier', 'Jordan Choquet', 'Adam Beliveau', 'Jonathan Degoede', 'Joaquin Faundez Flores']]
-    timelogs.to_csv(join(dirname(__file__), f'timelogs_{datetime.date.today()}'), index=False)
+    timelogs.to_csv(join(dirname(__file__), f'timelogs_{datetime.date.today()}.csv'), index=False)
 
 
 def calculate_estimated_time(row):
-    total_time, progress = convert_to_seconds(row['Total time spent']), row['Progress']
-    return str(int(round((total_time / (int(progress) / 100)) / 3600, 0))) + 'h'
+    total_time = convert_to_seconds(row['Total time spent']) if row['Total time spent'] else 0
+    progress = row['Progress'] if row['Progress'] else 0
+    try:
+        estimated_time = str(int(round((total_time / (int(progress) / 100)) / 3600, 0))) + 'h'
+    except ZeroDivisionError:
+        estimated_time = '0h'
+    return estimated_time
 
 
 def add_id_to_summary(row):
